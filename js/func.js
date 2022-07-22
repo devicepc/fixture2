4eed9a569faf01aef0e42f527a79a2772d9c5822
devicepc <69884110+devicepc@users.noreply.github.com>
2022-07-22 00:12:28 -0300

    Add files via upload

--- js/func.js
+++ js/func.js
@@ -6,9 +6,6 @@ initCountries();
 const print = document.getElementById("print");
 const refresh = document.getElementById("refresh");
 
-print.addEventListener("click", function(event){
-    window.print();
-})
 
 refresh.addEventListener("click", function(event){
     location.reload();
@@ -19,4 +16,26 @@ function scrollUp(){
     if (currentScroll >0){
         window.scrollTo(0,0);
     }
+}
+
+
+$(document).ready(() => {
+    $('#print').click(function() {
+      document.getElementById("titleprint").style.display='block';
+      document.getElementById("titleprint2").style.display='block';
+        $.print('#zoneprint');
+        Functionretraso();
+    });
+   
+});
+
+// brorar luego de compara entradas
+
+function Functionretraso() {
+retrzomp4 = setTimeout(borarUserSelection, 6000 );
+console.log("borarEntradas");
+}
+function borarUserSelection(){
+document.getElementById("titleprint").style.display ="none"; 
+document.getElementById("titleprint2").style.display ="none"; 
 }
